07a104ed39124a3df3672d11f301a3bae393a959
sou0913 <usokisasa0913@gmail.com>
2019-12-29 23:11:45 +0900

    Dockerfileにてvimインストール

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,10 +3,9 @@
 require 'rails_helper'
 
 describe UsersController, type: :controller do
-  let(:user) {create(:user)}
-
   describe 'get #first' do
     it 'redirect to root path' do
+      user = create(:user)
       sign_in user
       get :first, params: { id: user }
       expect(response).to redirect_to('/')
@@ -20,6 +19,7 @@ describe UsersController, type: :controller do
   end
   describe 'get #index' do
     it "renders the :index template" do
+      user = create(:user)
       sign_in user
       get :index
       expect(response).to render_template :index
@@ -28,9 +28,14 @@ describe UsersController, type: :controller do
       get :index
       expect(response).to redirect_to(user_session_path)
     end
+    it 'assigns the requested users to @users' do
+      users = create_list(:user, 3)
+      get :index
+      expect(response).to render_template :index
   end
   describe 'get #show' do
     it 'renders the :show template' do
+      user = create(:user)
       sign_in user
       get :show, params: {id: user}
       expect(response).to render_template :show
@@ -40,6 +45,7 @@ describe UsersController, type: :controller do
       expect(response).to redirect_to(user_session_path)
     end
     it 'assigns the requested user to @user' do
+      user = create(:user)
       sign_in user
       get :show, params: {id: user}
       expect(assigns(:partner)).to eq user
@@ -47,9 +53,35 @@ describe UsersController, type: :controller do
   end
   describe 'get #edit' do
     it 'renders the :edit template' do
+      user = create(:user)
       sign_in user
       get :edit, params: {id: user}
       expect(response).to render_template :edit
     end
   end
+  describe 'get #favor' do
+    it 'renders the :favor template' do
+      user = create(:user)
+      sign_in user
+      get :favor, params: {id: user}
+      expect(response).to render_template :favor
+    end
+  end
+  describe 'get #be_favored' do
+    it 'renders the :be_favored template' do
+      user = create(:user)
+      sign_in user
+      get :be_favored, params {id: user}
+      expect(response).to render_template :be_favored
+    end
+  end
+  describe 'get #match' do
+    it 'renders the :match template' do
+      user = create(:user)
+      sign_in user
+      get :match, params {id: user}
+      expect(response).to render_template :match
+    end
+  end
+
 end
