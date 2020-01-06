139418285ab6e0aeceeaee5f47667ae66aac947e
sou0913 <usokisasa0913@gmail.com>
2020-01-06 16:22:20 +0900

    cronのtask編集

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,6 @@
 set :output, "log/crontab.log"
 
+job_type :rake, 'cd :path && :environment_variable=:environment bundle exec /usr/local/bundle/bin/rake :task :output'
 
  every 1.hours do
    rake "init:relations"
